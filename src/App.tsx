61ae19eda7737891a5a74740cc29aa4ba2688819
Elena Semeshyna <81982732+LenaXmi@users.noreply.github.com>
2023-02-01 20:05:57 +0100

    add initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import TodoList from './components/TodoList'
 
-const App:React.FC = ()=> {
+const App: React.FC = () => {
+  
+  const todos = [{id:'1',title:'text'}]
   return (
     <div className="App">
-     
+      <TodoList todos={ todos} />
     </div>
   );
 }
